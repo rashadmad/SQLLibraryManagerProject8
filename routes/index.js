1802a8532c98f4dc735c8e5a9475e1bbedf6533a
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-07-31 13:34:18 -0700

    add routes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,71 @@ var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;  
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(Book)
-  //res.render('index', { title: 'Express' });
-});
+//modular async function to allow other functions async nature
+const asyncHandler = (cb) => {
+  return async (req,res,next) => {
+      try {
+        await cb(req,res,next);  
+      }
+      catch(err) {
+          res.render('./errors/error', {
+              errors: err
+          })
+      }
+  }
+}
 
+//Home route should redirect to the /books route.
+router.get('/', asyncHandler(async (req, res, next) => {
+  res.redirect('/books');
+}));
+
+//Shows the full list of books.
+router.get('/books', asyncHandler(async (req, res, next) => {
+  res.render('index', {
+      booksToView: [...books]
+  })
+}));
+
+//Shows the create new book form.
+router.get('/books/new', asyncHandler(async (request, response, next) => {
+  response.render('new-book')
+}));
+
+//Posts a new book to the database.
+router.post('/books/new', asyncHandler(async (request, response, next) => {
+  response.send("New Book has been created")
+  response.render('index')
+}));
+
+//Shows book detail form.
+router.get('/book_detail/:id', asyncHandler(async (request, response, next) => {
+  response.render('book_detail', {
+      _booksToView: books[request.params.id],
+      get booksToView() {
+          return this._booksToView;
+      },
+      set booksToView(value) {
+          this._booksToView = value;
+      },
+  })
+}));
+
+//Updates book info in the database.
+router.post('/books/:id', asyncHandler(async (request, response, next) => {
+      response.render('book_detail', {
+      _booksToView: books[request.params.id],
+      get booksToView() {
+          return this._booksToView;
+      },
+      set booksToView(value) {
+          this._booksToView = value;
+      },
+  })
+}));
+
+//Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting.
+router.post('/books/:id/delete', asyncHandler(async (request, response, next) => {
+  response.render('index')
+}));
 module.exports = router;
