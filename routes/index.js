08eb816607233a334beff8af2a72051e35b8e561
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-07-31 12:36:43 -0700

    add styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;  
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(Book)
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
