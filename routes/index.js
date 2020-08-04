490c25d355742f465be09deec6216a32a6867ef2
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-08-03 23:14:14 -0700

    add validation to update and create forms

--- routes/index.js
+++ routes/index.js
@@ -36,8 +36,17 @@ router.get('/books/new', asyncHandler(async (req, res) => {
 
 //CREATE Posts a new book to the database.
 router.post('/books/new', asyncHandler(async (req, res) => {
-    const newBook = await Books.create(req.body)
-    res.redirect("/books/")
+        try {
+            const newBook = await Books.create(req.body)
+            res.redirect("/books/")
+        } catch (error) {
+        if(error.name === "SequelizeValidationError") { // checking the error
+            book = await Books.build(req.body);
+            res.render("/books/new", { book, errors: error.errors})
+        } else {
+            throw error; // error caught in the asyncHandler's catch block
+        }  
+    }
 }));
 
 //READShows book detail form.
@@ -49,19 +58,31 @@ router.get('/books/:id', asyncHandler(async (req, res) => {
             book: selectedBook
         })
     } else {
+        res.send(404)
         res.render('page_not_found');
     } 
 }));
 
 //UPDATE book info in the database.
 router.post('/books/:id', asyncHandler(async (req, res) => {
-    const book = await Books.findByPk(req.params.id);
-    if(book){
-        await book.update(req.body);
-        res.redirect("/books/" + book.id)
-    } else {
-        res.render('page_not_found');
-    } 
+    try {
+        const book = await Books.findByPk(req.params.id);
+        if(book){
+            await book.update(req.body);
+            res.redirect("/books/" + book.id)
+        } else {
+            res.send(404)
+            res.render('page_not_found');
+        } 
+    } catch (error) {
+        if(error.name === "SequelizeValidationError") {
+          book = await Books.build(req.body);
+          book.id = req.params.id; // make sure correct article gets updated
+          res.render("articles/edit", { book, errors: error.errors, title: "Edit Article" })
+        } else {
+          throw error;
+        }
+      }
 }));
 
 //show DELETE form to make sure users really wants to delete a book
@@ -72,6 +93,7 @@ router.get('/books/:id/delete', asyncHandler(async (req, res) => {
             book: bookToBeDeleted 
         })
     } else {
+        res.send(404)
         res.render('page_not_found');
     } 
 }));
@@ -83,6 +105,7 @@ router.post('/books/:id/delete', asyncHandler(async (req, res) => {
         await bookToBeDeleted.destroy(req.body);
         res.redirect('/')
     } else {
+        res.send(404)
         res.render('page_not_found');
     } 
 }));
